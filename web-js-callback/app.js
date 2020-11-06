b116469331463d0a11742439c204964c95c8c74d
francisco <207478@iessierradeguara.com>
2020-11-06 16:34:27 +0100

    json

--- web-js-callback/app.js
+++ web-js-callback/app.js
@@ -5,6 +5,11 @@ function imprimirEn2(txt){
     document.getElementById("resultado2").innerHTML = txt;
 }
 
+function imprimirFin(){
+    document.getElementById("resultado1").innerHTML = "FIN";
+    document.getElementById("resultado2").innerHTML = "FIN";
+}
+
 // Esta funcion, multiplica los dos primeros parametros y llama a la funcion
 // pasada com 3er parametro
 
@@ -18,3 +23,5 @@ multiplicacionConCallback(34,7,imprimirEn2);
 multiplicacionConCallback(3,3,alert)
 multiplicacionConCallback(5,4,console.error)
 multiplicacionConCallback(5,6,console.log)
+setTimeout(imprimirFin,3000);
+setTimeout(multiplicacionConCallback,1000,5,5,imprimirEn1);
