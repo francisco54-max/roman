670896d2f01ecaf27d337f25a0df911e13761224
francisco <207478@iessierradeguara.com>
2020-11-16 21:11:47 +0100

    ejercicios

---
+++ t2-EjerciciosJS/ejercicios/ej3.js
@@ -0,0 +1,12 @@
+//valor 2
+alert("Empieza el ejercicio 3");
+// Crea una funcion llamada cierto sin parámetros que siempre devuelva true.
+function cierto(){
+    return false;
+}
+console.log(cierto);
+
+// Crea una función llamada falso sin parámetros que utilice la función anterior para devolver siempre falso
+
+
+
