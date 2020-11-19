385b2dfefd7f960180ac4cef30f7c0cb7caa29a9
francisco <207478@iessierradeguara.com>
2020-11-19 10:21:29 +0100

    ejercicios

---
+++ ejerciciosJS/t2-EjerciciosJS/ejercicios/ej2.js
@@ -0,0 +1,15 @@
+// valor 1
+alert("Empieza el ejercicio 2");
+// Crea una variable numérica.
+num1 = 9;
+
+// Crea una cadena de texto con el valor 25.
+str = 25;
+
+// Imprime la suma de ambas mediante un alert.
+alert(num1 + str);
+// Crea una variable booleana.
+booleano = true;
+
+// Imprime en la consola la suma de las 3 variables.
+alert(num1 + str + booleano);
