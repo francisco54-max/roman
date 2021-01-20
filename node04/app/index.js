81fc6a8c97c41373f94e8d8569ac45237fcf0168
francisco <207478@iessierradeguara.com>
2021-01-20 18:11:54 +0100

    nodemon

--- node04/app/index.js
+++ node04/app/index.js
@@ -1,1 +1,21 @@
-console.log("Hola mundo");
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set ("puerto", 3000);
+
+app.get('/', 
+    function (req,res) {
+        let pathToIndex = 
+            path.join(__dirname, "public", "index.html");
+        res.sendFile(__dirname);
+    }
+    
+    );
+
+app.listen(app.get("puerto"), inicioServidor());
+
+function inicioServidor() {
+    console.log(
+        `Servidor iniciado en http://localhost:${app.get("puerto")}`);
+}
