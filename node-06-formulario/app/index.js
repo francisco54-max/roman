d54dcb9232fd9a2f894e731ab725f8563c0deb62
francisco <207478@iessierradeguara.com>
2021-01-25 21:11:30 +0100

    formulario

---
+++ node-06-formulario/app/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('puerto', 3000);
+
+app.get('/', 
+    function (req,res) {
+        let pathToIndex = 
+            path.join(__dirname, "form.html");
+            
+        res.sendFile(pathToIndex);      
+    }
+);
+
+
+
+app.listen(app.get('puerto'), inicioServidor());
+
+function inicioServidor() {
+    console.log(
+        `Servidor iniciado en 
+http://localhost:${app.get("puerto")}`);
+}
